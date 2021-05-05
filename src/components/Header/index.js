b745a27a127965e162aec6172243d964472e2ec6
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-05-04 22:23:54 -0700

    changes to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,8 +5,8 @@ import selfie from "../../images/selfie.jpeg"
 
 function Header() {
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <figure className="image is-96x96">
+    <nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+      <figure className="image is-64x64">
         <img 
           className="selfie is-rounded"
           src={selfie}
@@ -14,8 +14,8 @@ function Header() {
           alt="Stephanie Zavala"
         />
       </figure>
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-end">
+      <div className="navbar-menu">
+        <div className="navbar-end has-navbar-fixed-top">
           <Link
             className="navbar-item"
             to="/"
