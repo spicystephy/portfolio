a1417d2fc292cb32010c247f1559049445a6997b
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-05-05 23:34:48 -0700

    style updates

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,24 +6,27 @@ function About() {
     <div className="section">
       <Container>
         <div className="content is-medium">
-          <p>
-            I joined the U.S. Marine Corps in 2003. Upon being
-            medically retired for stage III colon cancer, I began to research
-            cancer and realized that there was a strong relationship between
-            some cancers and food. After finishing with chemotherapy, I moved
-            back home to pursue more knowledge in nutrition. I graduated from
-            the University of Arizona with a B.S. in Nutritional Sciences and a
-            minor in chemistry. I has worked several years teaching classes
-            and providing coaching for people who wanted to make changes for the
-            betterment of their health.
-          </p></div>
-          <div className="content is-medium">
-          <p>
-            Currently, I am attending a 12 week course that covers the full-stack, web-development
-            curriculum. Please check back for updates of realized projects as
-            well as a few of the projects I have worked on so far. Curriculum covered:
-          </p></div>
-          <div className="content is-medium">
+          <p style={{ padding:70 }}>
+            I joined the U.S. Marine Corps in 2003. Upon being medically retired
+            for stage III colon cancer, I began to research cancer and realized
+            that there was a strong relationship between some cancers and food.
+            After finishing with chemotherapy, I moved back home to pursue more
+            knowledge in nutrition. I graduated from the University of Arizona
+            with a B.S. in Nutritional Sciences and a minor in chemistry. I has
+            worked several years teaching classes and providing coaching for
+            people who wanted to make changes for the betterment of their
+            health.
+          </p>
+        {/* </div> */}
+        {/* <div className="content is-medium"> */}
+          <p style={{ padding:70 }}>
+            Currently, I am attending a 12 week course that covers the
+            full-stack, web-development curriculum. Please check back for
+            updates of realized projects as well as a few of the projects I have
+            worked on so far. Curriculum covered:
+          </p>
+        {/* </div> */}
+        <div style={{ padding:70 }}>
           <ul>
             <li>HTML</li>
             <li>CSS</li>
@@ -35,9 +38,9 @@ function About() {
             <li>Express</li>
             <li>Sequelize</li>
             <li>MERN Stack</li>
-          </ul></div>
-          
-        
+          </ul>
+        </div>
+        </div>
       </Container>
     </div>
   );
